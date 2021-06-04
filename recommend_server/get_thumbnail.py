d812ba5753d63e5c8e57ec0d00983a58f4ecf9ed
digdag1 <56144201+digdag1@users.noreply.github.com>
2021-06-04 17:31:46 +0900

    21.06.04
    thumbnail crawling

--- recommend_server/get_thumbnail.py
+++ recommend_server/get_thumbnail.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
-import datetime
 import csv
 import urllib.request
 
 
 
-data = pd.read_csv(r"bucket_list_ko2.csv")
+data = pd.read_csv("bucket_list_ko2.csv", encoding='cp949')
 selectdata = pd.DataFrame(data, columns=['title', 'quest_num']) #특정컬럼선택
 
 # selenium에서 사용할 웹 드라이버 절대 경로 정보
@@ -18,7 +16,8 @@ driver = webdriver.Chrome(chromedriver)
 # driver로 특정 페이지를 크롤링한다.
 driver.get('https://www.google.co.kr/imghp?hl=ko&ogbl')
 
-for index, row in range selectdata.iterrows() :
+for index, row in selectdata[63:].iterrows() :
+    driver.get('https://www.google.co.kr/imghp?hl=ko&ogbl')
     elem_search = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[1]/div/div[2]/input")
     elem_search.clear()
     elem_search.send_keys(row['title'] + ' + bucketlist.org')
@@ -26,8 +25,14 @@ for index, row in range selectdata.iterrows() :
     btn_login = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[1]/button')
     btn_login.click()
     
+    elem_image_little = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div[1]/div[1]/span/div[1]/div[1]/div[1]/a[1]/div[1]/img')
+    elem_image_little.click()
+
     elem_image = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img')
-    url = elem_image.getsrc()
-    imgName = "C:\wthumbnail\" + row['quest_num'] + '.jpg' 
+    url = elem_image.get_attribute("src")
+    print(url)
+    imgName = 'C:\img\\' + str(row['quest_num']) + '.jpg'  
     urllib.request.urlretrieve(url,imgName)
+
 print('다운완료')
+
