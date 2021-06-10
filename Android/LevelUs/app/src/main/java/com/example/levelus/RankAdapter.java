3441e413ca2050f4f8eae2b586c83af2a3875b88
HaSeungyeon <gktmdudl@korea.ac.kr>
2021-06-10 16:15:40 +0900

    랭킹 등수넣기 완료

--- Android/LevelUs/app/src/main/java/com/example/levelus/RankAdapter.java
+++ Android/LevelUs/app/src/main/java/com/example/levelus/RankAdapter.java
@@ -1,8 +1,12 @@
 package com.example.levelus;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,21 +16,30 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class RankAdapter extends RecyclerView.Adapter<RankAdapter.ViewHolder> {
     private ArrayList<UserAccount> rankList = new ArrayList<UserAccount>();
-
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        protected TextView text_name, text_rank;
+        protected TextView text_name, text_level, rank;
+        protected ImageView highest_rank_imageView;
+        protected Context context;
+        protected Drawable medal;
         public ViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+            this.rank = itemView.findViewById(R.id.rank);
             this.text_name = itemView.findViewById(R.id.text_name);
-            this.text_rank = itemView.findViewById(R.id.text_rank);
+            this.text_level = itemView.findViewById(R.id.text_level);
+            this.highest_rank_imageView = itemView.findViewById(R.id.highest_rank_imageView);
+//            gold_medal = context.getResources().getDrawable(R.drawable.gold_medal);
+//            silver_medal = context.getResources().getDrawable(R.drawable.silver_medal);
+//            bronze_medal = context.getResources().getDrawable(R.drawable.bronze_medal);
         }
     }
 
     public RankAdapter(ArrayList<UserAccount> rankList) {
         this.rankList = rankList;
+        Collections.sort(this.rankList);
     }
 
     @NonNull
@@ -40,12 +53,20 @@ public class RankAdapter extends RecyclerView.Adapter<RankAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull RankAdapter.ViewHolder holder, int position) {
+        int rank = holder.getAdapterPosition();
         holder.text_name.setText(rankList.get(position).getName());
-        holder.text_rank.setText(Integer.toString(rankList.get(position).getLevel()));
-        holder.text_rank.setOnClickListener(new View.OnClickListener() {
+        holder.text_level.setText(Integer.toString(rankList.get(position).getLevel()));
+//        holder.highest_rank.setImageDrawable(gold_medal);
+        switch (rank){
+            case 0: holder.highest_rank_imageView.setBackgroundResource(R.drawable.gold_medal); break;
+            case 1: holder.highest_rank_imageView.setBackgroundResource(R.drawable.silver_medal); break;
+            case 2: holder.highest_rank_imageView.setBackgroundResource(R.drawable.bronze_medal); break;
+            default : holder.rank.setText("rank. "+(rank+1)); break;
+        }
+        holder.text_level.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String clickedToken = holder.text_rank.getText().toString();
+                String clickedToken = holder.text_level.getText().toString();
                 Toast.makeText(v.getContext(),clickedToken,Toast.LENGTH_SHORT).show();
             }
         });
