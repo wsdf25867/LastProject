6222c16579c85c4251dc2394ece418de6698e6c9
digdag1 <56144201+digdag1@users.noreply.github.com>
2021-06-11 18:12:09 +0900

    Update recommend_list_signup.py

--- recommend_server/recommend_list_signup.py
+++ recommend_server/recommend_list_signup.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd 
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import firebase_admin
