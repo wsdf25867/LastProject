4c21e3b6a98abef9577dd38e897dfafdfc9521cd
HaSeungyeon <gktmdudl@korea.ac.kr>
2021-06-18 10:55:06 +0900

    배경 영상 넣음

--- Android/LevelUs/app/src/main/java/com/example/levelus/MainActivity.java
+++ Android/LevelUs/app/src/main/java/com/example/levelus/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
     private DatabaseReference databaseReference = firebaseDatabase.getReference("Level Us");
 //    private boolean state = firebaseAuth.
+    private String TAG = "VideoActivity";
+    private VideoView videoView;
+
     public static boolean isLoginSuccess = true;
     Button login_in,sign_in;
     @Override
@@ -39,6 +45,19 @@ public class MainActivity extends AppCompatActivity {
 //        Log.d("유저 Uid",firebaseUser.getUid().toString());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        //play video
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.water_video));
+
+        videoView.start();
+        //loop
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true); // 동영상 무한 반복. 반복을 원치 않을 경우 false
+            }
+        });
+
         login_in = findViewById(R.id.log_in);
         sign_in = findViewById(R.id.sign_in);
         login_in.setOnClickListener(new View.OnClickListener() {
